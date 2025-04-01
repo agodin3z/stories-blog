05337872ead6e15da0519383fcd0e24c293e9b2e
agodin3z <pistilloagodinez@gmail.com>
2025-04-01 07:55:45 -0600

    chore(pages): update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,13 +2,13 @@ import rss, {pagesGlobToRssItems} from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'nicdun.dev - blog',
-    description: 'Crafting the Digital Future with Web Development Wonders',
+    title: 'Stories by Pisto',
+    description: 'Un blog de historias/cuentos generados con IA',
     site: context.site,
     items: await pagesGlobToRssItems(
       import.meta.glob('./posts/*.{md,mdx}'),
     ),
     stylesheet: './rss/styles.xsl',
-    customData: `<language>en-us</language>`,
+    customData: `<language>es-sv</language>`,
   });
 }
