9b26d71a28d4dbb842acaee607e0ade7ea720e9c
agodin3z <pistilloagodinez@gmail.com>
2025-04-01 09:03:06 -0600

    style: format code with prettier

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,14 +1,12 @@
-import rss, {pagesGlobToRssItems} from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
-  return rss({
-    title: 'Stories by Pisto',
-    description: 'Un blog de historias/cuentos generados con IA',
-    site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./posts/*.{md,mdx}'),
-    ),
-    stylesheet: './rss/styles.xsl',
-    customData: `<language>es-sv</language>`,
-  });
+	return rss({
+		title: 'Stories by Pisto',
+		description: 'Un blog de historias/cuentos generados con IA',
+		site: context.site,
+		items: await pagesGlobToRssItems(import.meta.glob('./posts/*.{md,mdx}')),
+		stylesheet: './rss/styles.xsl',
+		customData: `<language>es-sv</language>`
+	});
 }
